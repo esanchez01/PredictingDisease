989c8523d155c0dd4d490baead50244c0c310f51
Ricky Sanchez <ens004@ucsd.edu>
2020-04-08 16:48:13 -0700

    added code to transform VCF to ML

---
+++ src/etl.py
@@ -0,0 +1,49 @@
+""" Data Ingestion & Wrangling
+
+etl.py [description]
+
+"""
+
+# Importing libraries
+import pandas as pd
+import numpy as np
+
+# Importing scrips
+import read_data as rd
+
+
+
+def prepare_vcf(fp):
+    """
+    Transforms a VCF file into a machine learning
+    read dataframe. Rows represent a specific sample
+    and contain binary values signifying whether
+    the sample has a particular SNP.
+    
+    :param fp: Filepath to VCF file
+    :returns: Dataframe
+    """
+    
+    # Reading VCF file into dataframe
+    vcf = rd.read_vcf(fp)
+    
+    # Creating identifier column
+    vcf['ID'] = vcf.apply(lambda x: x['#CHROM']+':'+str(x['POS']), axis=1)
+
+    # Dropiing unnecessary columns and transposing
+    drop_cols = ['#CHROM', 'POS', 'REF', 'ALT', 
+                 'QUAL', 'FILTER', 'INFO', 'FORMAT']
+    vcf = vcf.drop(drop_cols, axis=1).T
+
+    # Creating mappings for reference/alternate pairs
+    variant_map = {'0|0': 0, '1|0': 1, '0|1':2, '1|1':3}
+
+    # Wrangling data
+    vcf.columns = vcf.loc['ID', :]
+    vcf = vcf.drop('ID', axis=0).reset_index()
+    vcf.columns.name = None
+    vcf = vcf.rename({'index': 'Sample'}, axis=1).replace(variant_map)
+
+    return vcf
+
+
---
+++ src/read_data.py
@@ -0,0 +1,92 @@
+"""  Data Reader
+
+This script takes in VCF, BAM, and FASTQ files
+and converts them into a pandas data frame.
+
+"""
+
+# Importing libraries
+import pandas as pd
+import numpy as np
+import pysam
+import io
+from itertools import islice
+
+
+def read_vcf(fp):
+    """
+    Reads VCF file
+    
+    :param fp: String representing file path to VCF file
+    """
+    
+    # Read VCF line by line
+    with open(fp, 'r') as f:
+        lines = [l for l in f if not l.startswith('##')]
+        
+    # Convert lines to dataframe
+    vcf_df = pd.read_csv(io.StringIO(''.join(lines)), sep='\t')
+        
+    return vcf_df
+
+
+
+def read_bam(fp):
+    """
+    Reads BAM file
+    
+    :param fp: String representing file path to BAM file
+    """
+    
+    # Read file, convert to SAM
+    imported = pysam.AlignmentFile(fp, mode = 'rb')
+
+    # Store lines from SAM
+    sams = []
+    for i in imported:
+        sams.append(i.to_dict())
+
+    # Convert lines to dataframe
+    lines = {}
+    for k in sams[0].keys():
+          lines[k] = tuple(lines[k] for lines in sams)
+
+    sam_df = pd.DataFrame(lines)
+    
+    return sam_df
+
+
+
+def read_fastq(fp):
+    """
+    Reads FASTQ file
+    
+    :param fp: String representing file path to FASTQ file
+    """
+    
+    # Lists to store data frame data
+    identifiers, sequences = [], []
+    separators, quality_scores = [], []
+
+    # Reads every four lines, stores data
+    # NOTE: 1 observation = 4 lines in FASTQ
+    with open(fp, "r") as f:
+        while True:
+            next_4_lines = list(islice(f, 4))
+
+            if not next_4_lines:
+                break
+            else:
+                identifiers.append(next_4_lines[0].replace('\n', ''))
+                sequences.append(next_4_lines[1].replace('\n', ''))
+                separators.append(next_4_lines[2].replace('\n', ''))
+                quality_scores.append(next_4_lines[3].replace('\n', ''))
+
+    fastq_dict = {'identifier': identifiers, 
+                  'sequence': sequences, 
+                  'separator': separators, 
+                  'quality_score':quality_scores}
+
+    fastq_df = pd.DataFrame(fastq_dict)
+    
+    return fastq_df
