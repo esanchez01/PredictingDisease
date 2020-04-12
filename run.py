8d0ccbf478da7bcc04ed019d7214c921019f7a60
Allston Fojas <amfojas@ucsd.edu>
2020-04-11 20:55:42 -0700

    added code to filter snps

--- run.py
+++ run.py
@@ -1,11 +1,11 @@
-# #!/usr/bin/env python
+#!/usr/bin/env python
 
-# import sys
-# import json
-# import shutil
+import sys
+import json
+import shutil
 
-# sys.path.insert(0, 'src') # add library code to path
-# from etl import get_seasons, clean_seasons
+sys.path.insert(0, 'src') # add library code to path
+from etl import *
 # from model import driver
 
 
@@ -13,35 +13,46 @@
 # CLEAN_PARAMS = 'config/02-clean.json'
 # MODEL_PARAMS = 'config/03-model.json'
 
-# TEST_DATA_PARAMS = 'config/test-01-data.json'
+TEST_DATA_PARAMS = 'config/test-01-data.json'
 # TEST_CLEAN_PARAMS = 'config/test-02-clean.json'
 
 
-# def load_params(fp):
-#     with open(fp) as fh:
-#         param = json.load(fh)
+def load_params(fp):
+    with open(fp) as fh:
+        param = json.load(fh)
 
-#     return param
+    return param
 
 
-# def main(targets):
+def main(targets):
 
-#     # make the clean target
-#     if 'clean' in targets:
+    # make the clean target
+    if 'clean' in targets:
 #         shutil.rmtree('data/raw', ignore_errors=True)
-#         shutil.rmtree('data/cleaned', ignore_errors=True)
-#         shutil.rmtree('data/out', ignore_errors=True)
-#         shutil.rmtree('data/test', ignore_errors=True)
+        shutil.rmtree('data/cleaned', ignore_errors=True)
+        shutil.rmtree('data/out', ignore_errors=True)
+        shutil.rmtree('data/test', ignore_errors=True)
 
-#     # make the data target
-#     if 'data' in targets:
-#         cfg = load_params(DATA_PARAMS)
-#         [_ for _ in get_seasons(**cfg)]
+    # make the data target
+    if 'data' in targets:
+        print('running data target')
+        cfg = load_params(TEST_DATA_PARAMS)
+        get_data_test(**cfg)
+        
+    # make the process target
+    if 'process' in targets:
+        print('running process target')
+        cfg = load_params(TEST_DATA_PARAMS)
+        filter_vcf(**cfg)
+        
+    # make the test-project target
+    if 'test-project' in targets:
+        print('running test-project target')
+        cfg = load_params(TEST_DATA_PARAMS)
+        get_data_test(**cfg)
+        filter_vcf(**cfg)
 
-#         cfg = load_params(CLEAN_PARAMS)
-#         [_ for _ in clean_seasons(**cfg)]
-
-#     # make the test target
+    # make the test target
 #     if 'test' in targets:
 #         cfg = load_params(TEST_DATA_PARAMS)
 #         [_ for _ in get_seasons(**cfg)]
@@ -49,13 +60,14 @@
 #         cfg = load_params(TEST_CLEAN_PARAMS)
 #         [_ for _ in clean_seasons(**cfg)]
 
+    # make the model target
 #     if 'model' in targets:
 #         cfg = load_params(MODEL_PARAMS)
 #         driver(**cfg)
 
-#     return
+    return
 
 
-# if __name__ == '__main__':
-#     targets = sys.argv[1:]
-#     main(targets)
+if __name__ == '__main__':
+    targets = sys.argv[1:]
+    main(targets)
--- src/etl.py
+++ src/etl.py
@@ -7,9 +7,13 @@ etl.py [description]
 # Importing libraries
 import pandas as pd
 import numpy as np
+import os
+import gzip
+import shutil
+import subprocess as sp
 
 # Importing scripts
-import read_data as rd
+from src import read_data as rd
 
 
 
@@ -47,3 +51,141 @@ def prepare_vcf(fp):
     return vcf
 
 
+
+def get_table_vcf_test(chromosome, outpath, file_type):
+    """
+    Gets the VCF from the given testdata directory,
+    reads in the VCF as a DataFrame and converts it to a CSV file,
+    and saves the VCF and CSV files to a specified directory.
+
+    :param chromosome: The chromosome number
+    :param outpath: The path to save data
+    :param file_type: The file type of the genetic file
+    """
+    
+    # given test data input and corresponding output
+    in_fn  = './testdata/vcf/chr22_test.vcf.gz'
+    out_fn = './data/raw/vcf/chr22_test.vcf'
+    
+    # Check whether the specified path exists or not
+    path_exists = os.path.exists(outpath) 
+    if not path_exists:
+        print('making outpath:', outpath)
+        os.mkdir(outpath)
+    
+    # Check whether .vcf.gz file is already unzipped
+    unzip_file = os.path.exists(out_fn)
+    print('Does chr22_test.vcf exist?', unzip_file)
+    if not unzip_file:
+        print('converting .vcf.gz file to .vcf file')
+        with gzip.open(in_fn, 'rb') as f_in, open(out_fn, 'wb') as f_out:
+            shutil.copyfileobj(f_in, f_out)
+
+
+
+def filter_snps(vcf, gwas_data):
+    """
+    Filters the SNPs found in the VCF file
+    to just the relevant SNPs found in the GWAS
+    data source.
+
+    :param vcf: The input VCF containing all SNPs
+    :param gwas_data: The file containing relevant SNPs from the GWAS
+    :returns: String
+    """
+    
+    gwas_data['CHR_ID'] = pd.to_numeric(gwas_data['CHR_ID'], errors='coerce')
+    gwas_data = gwas_data.dropna(subset=['CHR_ID'])
+    gwas_data['CHR_ID'] = gwas_data['CHR_ID'].astype(int)
+    gwas_data['CHR_POS'] = gwas_data['CHR_POS'].astype(int)
+    filtered_data = gwas_data.merge(vcf, left_on='CHR_POS', right_on='POS')
+    filtered_snps = filtered_data['SNPS'].values.tolist()
+    filtered_snps_str = ', '.join(filtered_snps)
+    return filtered_snps_str
+
+
+
+def filter_vcf(vcf_path, maf, geno, mind, tsv_path, **kwargs):
+    """
+    Runs script shell file to run plink2 commands to
+    filter the VCF file.
+    
+    :param vcf_path: The file path to the input VCF file
+    :param maf: The minor allele frequency
+    :param geno: The value to filter variants with missing call rates exceeding its value
+    :param mind: The value to filter samples with missing call rates exceeding its value
+    :param kwargs: Extra key word arguments
+    :param tsv_path: The file path to the file containing relevant SNPs from the GWAS
+    """
+    
+    # calls helper functions to get the relevant SNPs as a string
+    vcf = rd.read_vcf(vcf_path)
+    gwas_data = rd.read_gwas_data(tsv_path)
+    filtered_snps_str = filter_snps(vcf, gwas_data)
+    
+    # opens script shell file
+    print('opening script')    
+    cmd_str = ("./src/filter_snps/filter_snps.sh" + " " + 
+               vcf_path + " " + filtered_snps_str + " " + 
+               str(maf) + " " + str(geno) + " " + str(mind))
+    proc = sp.Popen(cmd_str, stdout=sp.PIPE, stderr=sp.PIPE, shell=True)
+    
+    # runs script shell file
+    print('running script')
+    out_tuple = proc.communicate()
+    print('script finished running')
+
+
+
+# ---------------------------------------------------------------------
+# Driver Function
+# ---------------------------------------------------------------------
+def get_data_test(chromosomes, samples, outpath, file_types, **kwargs):
+    """
+    Reads in the desired data in test-params.json 
+    and uses the configuration to download 
+    the various file types and corresponding CSV files.
+
+    :param chromosomes: The chromosome numbers
+    :param samples: The sample numbers
+    :param outpath: The directory to which to save the data
+    :param file_types: The genetic file types
+    :param kwargs: Extra keyword arguments
+    """
+    
+    # Check whether the specified path exists or not
+    data_folder_path = './data'
+    data_folder_path_exists = os.path.exists(data_folder_path) 
+    if not data_folder_path_exists:
+        print('making data_folder_path')
+        os.mkdir(data_folder_path)
+    
+    # Check whether the specified path exists or not
+    path_exists = os.path.exists(outpath) 
+    if not path_exists:
+        print('making outpath:', outpath)
+        os.mkdir(outpath)
+        
+    # loop through each file type to create a saved data directory
+    for file_type in file_types:
+        # Check whether the specified savedir exists or not
+        savedir = outpath + '/' + file_type
+        savedir_exists = os.path.exists(savedir) 
+        if not savedir_exists:
+            print('making savedir:', savedir)
+            os.mkdir(savedir)
+    
+    # loop through each file type and chromosome to get table
+    for file_type in file_types:
+        print('get_data_test() - file_type:', file_type)
+        savedir = outpath + '/' + file_type
+        if file_type == 'vcf':
+            for chromosome in chromosomes:
+                get_table_vcf_test(chromosome, savedir, file_type)
+        elif file_type == 'bam':
+            for sample in samples:
+                get_table_bam_test(sample, savedir, file_type)
+        elif file_type == 'fastq':
+            for sample in samples:
+                get_table_fastq_test(sample, savedir, file_type)
+                get_table_fasta(sample, savedir, file_type)
