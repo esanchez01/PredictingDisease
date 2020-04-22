41712c17f62db050e692101c6ec74b19a1365bd0
michaelsorenson <michaelsorenson427@gmail.com>
2020-04-21 19:02:21 -0700

    Fixed git directory, VCF filtering and merging

--- run.py
+++ run.py
@@ -6,6 +6,8 @@ import shutil
 
 sys.path.insert(0, 'src') # add library code to path
 from etl import *
+from download_data import *
+from process_data import *
 # from model import driver
 
 
@@ -14,6 +16,9 @@ from etl import *
 # MODEL_PARAMS = 'config/03-model.json'
 
 TEST_DATA_PARAMS = 'config/test-01-data.json'
+DOWNLOAD_1000_GENOMES_PARAMS = 'config/download-1000-genomes-data.json'
+FILTER_MERGE_1000_GENOMES_PARAMS = 'config/filter-merge-1000-genomes-data.json'
+TEST_1000_GENOMES_PARAMS = 'config/test-1000-genomes-data.json'
 # TEST_CLEAN_PARAMS = 'config/test-02-clean.json'
 
 
@@ -32,6 +37,7 @@ def main(targets):
         shutil.rmtree('data/cleaned', ignore_errors=True)
         shutil.rmtree('data/out', ignore_errors=True)
         shutil.rmtree('data/test', ignore_errors=True)
+        shutil.rmtree('data/1000genomes', ignore_errors=True)
 
     # make the data target
     if 'data' in targets:
@@ -51,6 +57,23 @@ def main(targets):
         cfg = load_params(TEST_DATA_PARAMS)
         get_data_test(**cfg)
         filter_vcf(**cfg)
+
+    # make the download-1000-genomes target
+    if 'download-1000-genomes' in targets:
+        print('running download-1000-genomes target')
+        cfg = load_params(DOWNLOAD_1000_GENOMES_PARAMS)
+        download_ftp_data(**cfg)
+        cfg = load_params(FILTER_MERGE_1000_GENOMES_PARAMS)
+        filter_merge_by_chr(**cfg)
+
+    # make the test-1000-genomes target
+    if 'test-1000-genomes' in targets:
+        print('running test-1000-genomes target')
+        cfg = load_params(TEST_1000_GENOMES_PARAMS)
+        df = prepare_vcf(**cfg)
+        "Building Model..."
+        print(build_model(df))
+
 
     # make the test target
 #     if 'test' in targets:
